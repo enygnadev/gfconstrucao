d26fd7135d4989d92d681e7dbcc33305e50c960f
Enygnalab <116671864+enygnadev@users.noreply.github.com>
2025-11-14 12:11:56 +0000

    novo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image"
 
 interface LogoProps {
@@ -9,10 +11,12 @@ export function Logo({ className }: LogoProps) {
     <div className={className}>
       <Image
         src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/IMG_5535.jpg-zuS1UT8rXQ87kZ3sCgcUD1cGduuw3Q.jpeg"
-        alt="CHEVAL MIRROR Logo"
+        alt="GF Construtora Logo"
         width={40}
         height={40}
         className="rounded-full"
+        priority
+        unoptimized
       />
     </div>
   )
